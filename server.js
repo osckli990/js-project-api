68bbe0cec1159d394faa02e2d5c2c195a25f628f
Oscar Liljefors <oscar.lf@live.se>
2025-07-21 21:59:47 +0200

    start

--- server.js
+++ server.js
@@ -1,22 +1,42 @@
-import cors from "cors"
-import express from "express"
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import cors from "cors";
+import express from "express";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!")
-})
+  res.json({
+    message: "Welcome to Oscar's Thoughts Api!",
+    endpoints: listEndpoints(app),
+  });
+});
+
+app.get("/thoughts", (req, res) => {
+  res.json(thoughts);
+});
+
+app.get("/thoughts/:id", (req, res) => {
+  const { id } = req.params;
+  const thought = thoughts.find((t) => t.id === +id);
+
+  if (thought) {
+    res.json(thought);
+  } else {
+    res.status(404).json({ error: "Thought not found" });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
